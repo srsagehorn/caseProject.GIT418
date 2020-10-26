c333fc9a6300383e3be0c250157d485ccbfb0dda
Shannon Sagehorn <srsagehorn@gmail.com>
2020-10-26 15:01:52 -0500

    times displaying in table

--- script.js
+++ script.js
@@ -1,34 +1,61 @@
 // sets and array for priority levels to loop through later
 const levels = ["high", "mid", "low"]
+const times = ["morning", "mid-Day", "evening", "night", "no-Deadline"]
 
 // establishes class
 class ToDoItem {
-    constructor(title, category, priority) {
+    constructor(title, time, category, priority) {
       this.title = title;
+      this.time = time;
       this.category = category;
       this.priority = priority;
     }
   }
 
-// adds event listener to o buttoon
+// adds event listener to o button
 $("#todobtn").on('click', function (event) {
 event.preventDefault()
 // setting variable from the input values (both selected and typed in) for the constructor
 const title = $("#title").val()
 const category = $("#category").val()
+let time = []
+// console.log(category)
+
+// stops it from running if there is no title or category and alerts use\er
+if (!title) {
+alert("Please enter the name of your to do list item")
+return
+}
+
+if (category == "default") {
+  alert("Please select a category for your to do list item")
+  return
+}
+
+// goes through each priority and sees if they are checked
 levels.forEach(pri=> {
-    if($('#' + pri).is(':checked')) {
-        priority = pri
+  if($('#' + pri).is(':checked')) {
+      priority = pri
+  }
+})
+
+// 
+times.forEach(era=> {
+    if($('#' + era).is(':checked')) {
+        time.push(era.charAt(0).toUpperCase() + era.slice(1))
     }
 })
-// create a new class instance
-var newItem = new ToDoItem (title, category, priority)
 
+// create a new class instance
+var newItem = new ToDoItem (title, time, category, priority)
+
+console.log(newItem)
 // adds the new class content underneath the respective priority section
 levels.forEach(lev => {
     if (newItem.priority == lev) {
         $("#add"+ lev).before(`<tr class = ${lev}>
         <td>${newItem.title}</td>
+        <td>${newItem.time.toString().replace(",", " to ")}</td>
         <td>${newItem.category = newItem.category.charAt(0).toUpperCase() + newItem.category.slice(1)}</td>
         <td>${newItem.priority.charAt(0).toUpperCase() + newItem.priority.slice(1)}</td>
         </tr>`)
